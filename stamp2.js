752e8d99ec815c0d5aec4176034bf4515ac01f26
DAS <david@indyblue.com>
2017-11-26 14:24:30 -0500

    optimize template structure - match all patterns in a particular element/attr, not just first - use object for each replace, key=replace string, value=exec pattern - repeat structure, change recursion, don't get full Template - refactor certain "helper" functions out worker methods - new: get new copy of element (or to eval the path within element) - eval: follow path array down through element structure -- normally returns value -- can also perform "replace" operation if key/value are supplied - exec: uses supplied data to create new copy of template -- expand all expressions, iterate all repeats - append: -- normally adds whatever element(s) there are to another element -- if children flag is true, only append childNodes (for repeat)
    todo:
    - binding: changeable html elements (input, select, textarea) bind back
    - what about from model to DOM?
    - mustache areas:
    -- handle functions in data object?
    --- argless only?
    --- scalar args only?
    --- full args?
    -- operators? complex expressions?
    -- or just push these off into properties and functions?

--- stamp2.js
+++ stamp2.js
@@ -6,39 +6,133 @@ function Template(el, evalSelf=true) {
 	rv.element = el.cloneNode(true);
 	rv.patterns = [];
 	rv.repeats = [];
-	let rxMus = /{{\s*([^ \t}]+)\s*}}/i;
-	let rxRep = /^data-repeat-(\w+)$/;
+	let rxMus = /{{\s*([^ \t}]+)\s*}}/ig;
+	let rxRep = /^data-repeat-([^-]+)-?.*$/g;
+	let rxSplit = /[.\[\]]+/g;
 
-	(function findElementPatterns(e, path=[]) {
-		var patmatch=null;
-		if(e.nodeName==='#text' && (patmatch=rxMus.exec(e.nodeValue))) rv.patterns.push({
-			pattern: path.concat('nodeValue'), 
-			replace: patmatch[0],
-			expr: patmatch[1]
-		});
-		
-		let isRepeat=false
+	/*****************************************************************/
+	// helper functions
+	/*****************************************************************/
+	function uMatches(rx, str, pat=-1) {
+		if(!rx.global) {
+			console.error('rx no global flag! recompiling...', rx.source)
+			rx = new RegExp(rx.source, rx.flags+'g');
+		}
+		let am = [], m=rx.exec(''), i=0;
+		while(i<100 && (m=rx.exec(str)) !== null) {
+			am.push(pat<0?m:m[pat]);
+			i++;
+		}
+		return am;
+	}
+	function oMatches(rx, str, [k,v]=[0,1]) {
+		let m = uMatches(rx, str);
+		if(m.length===0) return null;
+		let obj = {};
+		for(let i of m)
+			if(!obj[i[k]]) obj[i[k]] = i[v];
+		return obj;
+	}
+	function newPat(path, str) {
+		let m = oMatches(rxMus, str);
+		if(m===null) return null;
+		return { pattern: path, replace: m };
+	}
+	function pcon(path, v=null) {
+		if(v===null) return path.concat('nodeValue');
+		else return path.concat('attributes', v, 'value');
+	}
+
+	/*****************************************************************/
+	// initialize pattern/repeat arrays
+	/*****************************************************************/
+	(function findElementPatterns(e, aPat, aRep, path=[]) {
+		var np=null;
+		//console.log(e.tagName||e.nodeName);
+		if(e.nodeName==='#text' && (np=newPat(pcon(path), e.nodeValue))!==null)
+			aPat.push(np);
+		//if(e.nodeName==='#text') console.log('nodeValue', e.nodeValue);
+
+		let tmpPath=path, tmpPat=aPat, tmpRep=aRep;
 		if(e.attributes) for(let a of e.attributes) {
-			if(patmatch=rxMus.exec(a.value)) rv.patterns.push({
-				pattern: path.concat('attributes', a.name, 'value'),
-				replace: patmatch[0],
-				expr: patmatch[1]
-			});
-			if(evalSelf && rxRep.test(a.name)) {
-				isRepeat=true;
-				rv.repeats.push({
-					pattern: path.concat('attributes', a.name, 'value'), 
+			//console.log('attr', a.name, a.value);
+			if((np=newPat(pcon(path,a.name),a.value))!==null)
+				aPat.push(np);
+			if(evalSelf && (np=rxRep.exec(a.name))) {
+				tmpPat=[]; tmpRep=[]; tmpPath=[];
+				aRep.push({
+					pattern: path.concat('attributes', a.name, 'value'),
 					value: a.value,
-					template: new Template(e, false)
+					base: np[1],
+					element: path,
+					patterns: tmpPat,
+					repeats: tmpRep
+					//template: new Template(e, false)
 				});
 			}
 		}
-		if(!isRepeat) for(let i=0;i<e.childNodes.length;i++) {
+		for(let i=0;i<e.childNodes.length;i++) {
 			let c = e.childNodes[i];
-			findElementPatterns(c, path.concat('childNodes',i));
+			findElementPatterns(c, tmpPat, tmpRep, tmpPath.concat('childNodes',i));
 		}
-	})(rv.element);
-	if(rv.patterns.length==0) delete rv.patterns;
-	if(rv.repeats.length==0) delete rv.repeats;
+	})(rv.element, rv.patterns, rv.repeats);
+
+	/*****************************************************************/
+	// worker methods
+	/*****************************************************************/
+	rv.new = function(e) {
+		if(Array.isArray(e)) e = rv.eval(rv.element, e);
+		return e.cloneNode(true);
+	};
+	rv.eval = function(path, data, k='', v='') {
+		let apath = path;
+		if(typeof apath==='string') apath = path.split(rxSplit);
+		let o = data;
+		for(let i=0;i<apath.length;i++){
+			let p = apath[i];
+			if(i==apath.length-1 && typeof o[p]==='string'
+				&& k!=='' && v!==''){
+				o[p] = o[p].replace(k, v);
+			}
+			else o = o[p]||'';
+		}
+		return o;
+	};
+	rv.empty = function(el) {
+		while(el.firstChild) el.removeChild(el.firstChild);
+	};
+	rv.exec = function(data, tmp, el) {
+		if(tmp===undefined) tmp = rv;
+		if(el===undefined) el = tmp.element;
+		let newe = rv.new(el);
+		for(let pat of tmp.patterns) {
+			for(let k in pat.replace) {
+				let v = rv.eval(pat.replace[k], data);
+				//console.log(k, v);
+				rv.eval(pat.pattern, newe, k, v);
+			}
+		}
+		for(let rep of tmp.repeats) {
+			let reps = [];
+			let rdata = rv.eval(rep.value, data);
+			let rel = rv.eval(rep.element, newe);
+			for(let d of rdata) {
+				let o = Object.assign({}, data, {[rep.base]:d});
+				reps.push(rv.exec(o, rep, rel));
+			}
+			rv.empty(rel);
+			//console.log(reps.map(x=> x.childNodes));
+			rv.append(rel, reps, true);
+		}
+		return newe;
+	};
+	rv.append = function(pe, els, children=false){
+		if(!Array.isArray(els)) els = [els];
+		for(let e of els){
+			if(children) while(e.firstChild) {
+				pe.appendChild(e.firstChild);
+			} else pe.appendChild(e);
+		}
+	}
 }
 
