be78461646accfdf8556cea7388849b5e54eaff4
DAS <david@indyblue.com>
2018-04-06 18:56:41 -0400

    add eventsource to 0ws add fetch spy to helpers

--- 0ws.js
+++ 0ws.js
@@ -5,16 +5,41 @@ const requestHandler = (request, response) => {
 	let url = request.url;
 	fpath = $path.join(__dirname, url);
 	fstat = $stat(fpath);
-	console.log(new Date(), url, fstat.isFile()?'f':fstat.isDirectory()?'d':'-')
-	if(fstat.isFile(fpath)) {
-		fs.createReadStream(fpath).pipe(response);
-	} else if(fstat.isDirectory()) {
-		response.write(`<html><head></head><body><h3>Directory listing of '${url}'</h3>`);
-		for(let i of fs.readdirSync(fpath)) response.write(
-			`<div><a href='${$join(url, i)}'>${i}</a></div>`);
-		response.write(`</body></html>`);
-		response.end();
-	} else response.end('404 not found!');
+	console.log(new Date(), url, fstat.isFile() ? 'f' : fstat.isDirectory() ? 'd' : '-')
+	if (fstat.isFile()) fs.createReadStream(fpath).pipe(response);
+	else if (fstat.isDirectory()) handleDir(response);
+	else if (url === '/$es') handleES(request, response);
+	else response.end('404 not found!');
+}
+
+function handleES(request, response) {
+	request.socket.setTimeout(1e12);
+	response.on('close', function (e) {
+		console.log('EventSource closed', e);
+	});
+	response.writeHead(200, {
+		'Content-Type': 'text/event-stream',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive'
+	});
+	response.write('\n');
+	setInterval(function () {
+		var output = fmtEsMsg('ping', { time: Date.now() });
+		response.write(output);
+	}, 2000);
+}
+function fmtEsMsg(event, data) {
+	var output = 'event: ' + event + '\n'
+		+ 'data: ' + JSON.stringify(data) + '\n\n';
+	return output;
+}
+
+function handleDir(response) {
+	response.write(`<html><head></head><body><h3>Directory listing of '${url}'</h3>`);
+	for (let i of fs.readdirSync(fpath)) response.write(
+		`<div><a href='${$join(url, i)}'>${i}</a></div>`);
+	response.write(`</body></html>`);
+	response.end();
 }
 
 const server = http.createServer(requestHandler);
@@ -24,10 +49,10 @@ server.listen(port, (err) => {
 });
 
 function $stat(fn) {
-	if(fs.existsSync(fn)) return fs.lstatSync(fpath);
-	else return { 
-		isFile() { return false;}, 
-		isDirectory() { return false;}
+	if (fs.existsSync(fn)) return fs.lstatSync(fpath);
+	else return {
+		isFile() { return false; },
+		isDirectory() { return false; }
 	};
 };
-function $join(a,b) { return $path.posix.normalize($path.posix.join(a,b)); }
+function $join(a, b) { return $path.posix.normalize($path.posix.join(a, b)); }
--- helpers.js
+++ helpers.js
@@ -4,6 +4,20 @@ if (!Element.prototype.matches) {
     Element.prototype.matches = Element.prototype.msMatchesSelector;
 }
 
+(function() {
+	var qfetch = window.fetch;
+	window.fetch = function() {
+		var fargs = arguments;
+		return qfetch.apply(window, arguments).then(function(resp) {
+			console.log(fargs, resp);
+			if(resp.clone) resp.clone().text().then(t=> {
+				console.log(t.substr(0,200));
+			});
+			return resp;
+		});
+	};
+})();
+
 /* 
 // this method of wrapping the callback makes it nearly impossible 
 // to use removeEventListener. Should I create an array?
@@ -19,4 +33,40 @@ Element.prototype.on = function(event, filter, cb) {
     }
     this.addEventListener(event, cbw);
 };
+
+			ajaxCallback = function(callback){
+				XMLHttpRequest.qwerCallback = callback;
+				
+				if(XMLHttpRequest.prototype.addSpy===true) return;
+				XMLHttpRequest.prototype.addSpy = true;
+				var oldSend = XMLHttpRequest.prototype.send;
+				XMLHttpRequest.prototype.send = function(data){
+					this.qwerData = data;
+					return oldSend.apply(this, arguments);
+				}
+				var oldOpen = XMLHttpRequest.prototype.open;
+				XMLHttpRequest.prototype.open = function(method, url){
+					var retval = oldOpen.apply(this, arguments);
+					XMLHttpRequest.qwerCallback(this, method, url );
+					return retval;
+				}
+			}
+			
+			ajaxCallback(function(xhr, method, url){
+					console.log('one', method, url, xhr.qwerData);
+				if(!(url==="/svc/rr/accounts/secure/v1/account/activity/card/list")) return;
+				let rsc = xhr.onreadystatechange;
+				xhr.onreadystatechange = function(e) {
+					if(typeof rsc==='function') rsc.apply(this, arguments);
+					console.log('two', method, url, xhr.qwerData);
+					if(xhr.readyState===4 && xhr.response!==''
+						&& /statementPeriodId=ALL/.test(xhr.qwerData) ) {
+						var data = JSON.parse(xhr.response)
+						console.log('readystate');
+						r(data);
+					}
+				}
+			});
+
+
 */
