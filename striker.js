cbf1145639a6d6e99887926ee28abc5c06371814
DAS <david@indyblue.com>
2018-04-20 13:42:56 -0400

    make repeat element removeable - remove keyword detector - if remove,   - insert all children before element   - then set  remove element   - then reset "e" to lastChild - change iteration logic to use nextSibling instead of for loop - works better than for loop with the new repeat/remove
    - example of the repeat/remove in striker_test

--- striker.js
+++ striker.js
@@ -2,7 +2,7 @@
 function Striker() {
     var t = this, sTemplate = 'data-template', sPrefix = 'data-prefix',
         rxSplit = /[.\[\]]+/g, rxPat = /{{\s*([^ \t}]+)\s*}}/ig,
-        rxRep = /^data-repeat-([^-]+)-?.*$/;
+        rxRep = /^data-repeat-([^-]+)-?(.*)$/;
 
     t.exec = function (e, data, removeParent, prefixes) {
         if ((e = elCheck(e)) === undefined) return;
@@ -15,14 +15,23 @@ function Striker() {
         if (!eChildren.length) eChildren = [eChildren];
         while (eChildren.length > 0) eParent.appendChild(eChildren[0]);
     };
+    t.before = function (eRef, eChildren) {
+        var eParent;
+        if (!eChildren.length) eChildren = [eChildren];
+        if ((eRef = elCheck(eRef)) === undefined) return;
+        if ((eParent = elCheck(eRef.parentElement)) === undefined) return;
+        while (eChildren.length > 0) eParent.insertBefore(eChildren[0], eRef);
+    };
     t.expend = function (eParent, e, data, removeParent, prefixes) {
         if ((eParent = elCheck(eParent)) === undefined) return;
         var e2 = t.exec(e, data, removeParent, prefixes);
         t.append(eParent, e2);
+        return eParent;
     };
 
     function traverse(e, data, prefixes) {
-        var tmpName = null, pat = null, rarray = null, rname, rdata, tmpName;
+        var tmpName = null, pat = null, rarray = null, rname, rdata,
+            tmpName, rremove = false;
         if ((e = elCheck(e)) === undefined) return;
         if (e.nodeName === '#text') {
             var cb = cbPat.bind(e.parentNode, data, '>', prefixes);
@@ -36,6 +45,7 @@ function Striker() {
             else if ((pat = rxRep.exec(a.name)) !== null) {
                 rname = pat[1];
                 rarray = cbPat(data, '>', prefixes, '', a.value);
+                if (pat[2] === 'remove') rremove = true;
             } else {
                 var cb = cbPat.bind(e, data, a.name, prefixes);
                 a.value = a.value.replace(rxPat, cb);
@@ -53,15 +63,28 @@ function Striker() {
                 (rdata = { '^': data })[rname] = rarray[i];
                 t.expend(e, tmpName, rdata, true, prefixes);
             }
+            if (rremove) {
+                var newE = e.lastChild;
+                t.before(e, e.childNodes);
+                e.remove();
+                e = newE;
+            }
         } else {
             if (tmpName !== null) t.expend(e, tmpName, data, true, prefixes);
-            else for (var i = 0; i < e.childNodes.length; i++)
-                traverse(e.childNodes[i], data, prefixes);
+            else {
+                var ce = e.firstChild;
+                while (ce != null) {
+                    ce = traverse(ce, data, prefixes);
+                    if (ce == null) break;
+                    ce = ce.nextSibling;
+                }
+            }
         }
+        return e;
     }
     function cbPat(data, name, pfx, all, path) {
         if (typeof path === 'string') path = path.split(rxSplit);
-        var pfx = (typeof pfx === 'string' ? pfx.split(/,/g) : pfx);
+        var pfx = (typeof pfx === 'string' ? pfx.split(/,/g) : pfx) || '';
         var e = this, o = data;
         for (var i = 0; i < path.length; i++) {
             var p = path[i];
